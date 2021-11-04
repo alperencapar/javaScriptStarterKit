a11aabcf54315bb0e5f35a05c5b74b91ebb9d984
alpcusta <alperencapar2@gmail.com>
2021-11-04 06:15:51 +0300

    There is bug in the findPrime function. Added new prime number function(to 1000)

--- src/homeworks/day2.js
+++ src/homeworks/day2.js
@@ -23,4 +23,14 @@ function findPrime(...numbers){
     console.log(`\n\n\nAsal Olmayan Sayılar:\n${nonPrimeNumbers}`)
 }
 
-findPrime(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46)
+// findPrime(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46)
+
+function findPrimeTo1000(callback){
+    let nums = []
+    for(let i = 0; i<1000; i++){
+        nums.push(i)
+    }
+
+    callback(...nums)
+}
+findPrimeTo1000(findPrime)
